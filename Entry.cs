a5cd6d00a192c900d168a00e5c44e31b831c4967
VAST <114198896+vast1337x@users.noreply.github.com>
2023-05-09 19:53:13 +0700

    Add files via upload

---
+++ Entry.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using Veylib.CLIUI;
+
+namespace LithiumNukerV2
+{
+	// Token: 0x02000004 RID: 4
+	internal class Entry
+	{
+		// Token: 0x0600000B RID: 11 RVA: 0x000028AC File Offset: 0x00000AAC
+		private static void parseArgs(string[] args)
+		{
+			for (int i = 0; i < args.Length; i++)
+			{
+				string a = args[i].ToLower();
+				if (!(a == "--debug"))
+				{
+					if (!(a == "--token"))
+					{
+						if (!(a == "--guild"))
+						{
+							if (!(a == "--threads"))
+							{
+								if (!(a == "--connection-limit"))
+								{
+									Entry.core.WriteLine(new object[]
+									{
+										Color.Red,
+										"Invalid argument: " + args[i].ToLower()
+									});
+								}
+								else
+								{
+									i++;
+									int connectionLimit;
+									if (!int.TryParse(args[i], out connectionLimit))
+									{
+										Entry.core.WriteLine(new object[]
+										{
+											Color.Red,
+											"--connection-limit argument value invalid"
+										});
+									}
+									Settings.ConnectionLimit = connectionLimit;
+								}
+							}
+							else
+							{
+								i++;
+								int threads;
+								if (!int.TryParse(args[i], out threads))
+								{
+									Entry.core.WriteLine(new object[]
+									{
+										Color.Red,
+										"--threads argument value invalid"
+									});
+								}
+								Settings.Threads = threads;
+							}
+						}
+						else
+						{
+							i++;
+							long value;
+							if (!long.TryParse(args[i], out value))
+							{
+								Entry.core.WriteLine(new object[]
+								{
+									Color.Red,
+									"--guild argument value invalid"
+								});
+							}
+							Settings.GuildId = new long?(value);
+						}
+					}
+					else
+					{
+						i++;
+						Settings.Token = args[i];
+					}
+				}
+				else
+				{
+					Settings.Debug = true;
+				}
+			}
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00002A34 File Offset: 0x00000C34
+		private static void Main(string[] args)
+		{
+			Core.StartupProperties startProperties = new Core.StartupProperties
+			{
+				MOTD = "fuck skids | verlox & russian heavy on top",
+				ColorRotation = 260,
+				SilentStart = true,
+				LogoString = Settings.Logo,
+				DebugMode = Settings.Debug,
+				Author = new Core.StartupAuthorProperties
+				{
+					Url = "verlox.cc & russianheavy.xyz",
+					Name = "verlox & russian heavy"
+				},
+				Title = new Core.StartupConsoleTitleProperties
+				{
+					Text = "Lithium Nuker V2"
+				}
+			};
+			Entry.core.Start(startProperties);
+			Entry.parseArgs(args);
+			ServicePointManager.DefaultConnectionLimit = Settings.ConnectionLimit;
+			ServicePointManager.Expect100Continue = false;
+			Picker.Choose();
+		}
+
+		// Token: 0x0400000F RID: 15
+		public static Core core = Core.GetInstance();
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LithiumNukerV2
+{
+	// Token: 0x02000002 RID: 2
+	public class Settings
+	{
+		// Token: 0x04000001 RID: 1
+		public static bool Debug = false;
+
+		// Token: 0x04000002 RID: 2
+		public static readonly string Logo = "\r\n            ▒▒▒          ░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒      ▒░▒▒▒      ▒▒▒                                 ▒░░░▒\r\n          ▒▒██▒         ▒▒▒▒▒░▒███████████████████░▒▒▒▒▒▒▒████▒     ▒▒▒█▒       ▒▒▒     ▒▒     ▒ ▒      ▒████▒\r\n        ▒█████▒        ▒████▒   ▒▒▒▒▒█████▒▒       ▒▒░▒▒▒▒████▒    ▒████▒    ░▒▒░░▒   ▒░█▒   ▒░▒░▒      ▒████▒\r\n       ▒█████▒        ▒▒████▒      ░▒████▒▒      ▒░███▒▒▒█████▒   ▒▒████▒   ▒████▒   ▒██▒▒  ▒▒███▒▒    ▒████▒▒\r\n      ▒▒████▒         ▒████▒▒      ▒████░▒      ▒░████▒▒▒████▒    ▒████▒▒   ▒███▒  ▒▒███▒   ▒█████▒ ▒▒█████▒▒\r\n     ░▒████▒          ▒████▒      ▒▒████▒   ▒▒▒░█████▒▒▒████▒▒    ▒████▒  ▒▒███▒▒  ▒████▒   ▒█████▒▒██████▒▒\r\n     ▒░████▒         ░▒███▒       ▒███░▒   ▒▒▒░████████████▒▒▒▒  ▒▒███▒   ▒████▒  ▒█████▒  ▒░████████████▒▒\r\n     ▒████▒          ▒▒███▒      ▒░███▒▒      ▒████▒▒▒▒████▒▒▒   ▒▒██░▒   ▒███▒  ▒▒████▒   ▒███▒▒███▒▒███▒\r\n  ▒▒▒█████▒▒░░░▒░█░▒▒▒███▒       ▒▒██▒▒      ░▒███▒  ▒███▒▒      ▒███▒    ▒███▒ ▒▒████▒   ▒▒███▒▒▒█▒░▒██▒▒\r\n ░ ░▒█████████████░▒▒▒██▒▒       ▒███▒       ▒███▒▒  ▒███▒       ▒██▒▒    ▒░█████████▒▒   ▒██▒▒▒░▒▒▒▒▒█▒▒\r\n   ▒▒▒░░▒▒▒▒▒▒       ▒░▒▒        ▒░██▒       ▒██░▒   ▒██▒░       ▒░▒░       ▒░█████░▒     ▒█▒       ▒░▒▒\r\n                    ▒░▒          ▒▒█▒        ▒█▒▒   ▒░▒▒        ▒▒▒                                 ▒▒▒\r\n                                 ▒░▒▒       ▒▒▒     ▒▒░\r\n                                 ▒▒▒";
+
+		// Token: 0x04000003 RID: 3
+		public static string Token;
+
+		// Token: 0x04000004 RID: 4
+		public static long? GuildId = null;
+
+		// Token: 0x04000005 RID: 5
+		public static int Threads = 40;
+
+		// Token: 0x04000006 RID: 6
+		public static int ConnectionLimit = 25;
+
+		// Token: 0x04000007 RID: 7
+		public static readonly string WebhookName = ".gg/lithium runs cord";
+
+		// Token: 0x04000008 RID: 8
+		public static readonly string AvatarUrl = "https://camo.githubusercontent.com/450b75468a748fbd8e4b3116c378cc9cfdcadd8b0b0e676821a6f873fcb85f53/68747470733a2f2f7665726c6f782e63632f7261772f535865475144";
+	}
+}
